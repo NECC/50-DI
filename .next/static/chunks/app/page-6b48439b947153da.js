6d871ef66cb3c6fe90c2167a5a48e16b4debf5d4
D2G8 <digo83389@gmail.com>
2026-01-14 18:05:28 +0000

    Build fix

---
+++ .next/static/chunks/app/page-6b48439b947153da.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[974],{2767:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,9626,23))}},e=>{e.O(0,[626,544,659,358],()=>e(e.s=2767)),_N_E=e.O()}]);
